3e66290773d76ccdca46cf8aa4cce2133bde3989
anactazia <anactazia@hotmail.com>
2013-06-01 13:19:38 +0200

    myBumperCar v.1.0

---
+++ bumperCarMultiPlayer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Bumper Cars</title>
+<link rel="stylesheet" href="css/style.css">
+<script language="JavaScript" src="js/game.js"></script>
+<script language="JavaScript" src="js/clientMultiPlayer.js"></script>
+
+
+</head>
+
+<body>
+<div class="gameArea">
+<p class="headline">BumperCar</p>
+<p class="instructions">Drive the car with the arrow keys! (up = forward, left = left, right = right)</p>
+<canvas id="BumperCanvas">
+Your browser does not support HTML canvas!
+</canvas>
+<audio controls id="music">
+  <source src="audio/bg_music.mp3" />
+</audio>
+</div>
+</body>
+</html>
